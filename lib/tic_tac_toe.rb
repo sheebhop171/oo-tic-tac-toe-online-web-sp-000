c14ad4e5cc8a7500f3fd543188b93b946140d15f
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-24 21:43:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ class TicTacToe
 
   def position_taken?(user_input)
       binding.pry
-    if index == "X" || index == "O"
+    if user_input == "X" || index == "O"
       true
     else index != " "
       false
