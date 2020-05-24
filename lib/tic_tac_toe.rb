4ee90875c98771abe10edda0a4b578b2b4be01bb
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-24 22:11:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,13 +53,13 @@ class TicTacToe
   def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input_to_index(input)
-    if valid_move?(board,index)
-      move(board,index,current_player(board))
-      display_board(board)
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      move(index, token)
+      display_board
     else
       turn(board)
-    end  
+    end
   end
 
   def turn_count(board)
